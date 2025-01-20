ae918b0cff36a83c2bb5d26c3374f95269cbbd32
JacobSchroder <36925561+JacobSchroder@users.noreply.github.com>
2025-01-20 20:50:13 +0100

    updated useCalendarView

--- hooks/useCalendarView.ts
+++ hooks/useCalendarView.ts
@@ -1,12 +1,21 @@
+'use client';
 import { useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { z } from 'zod';
 
-export const zCalendarView = z.enum(['day', 'week', 'month']).catch('week');
+export const zCalendarView = z.enum(['day', 'week', 'month']);
 export type CalendarView = z.infer<typeof zCalendarView>;
 
 export function useCalendarView() {
+  const [view, setView] = useState<CalendarView>('week');
   const searchParams = useSearchParams();
-  const view = zCalendarView.parse(searchParams.get('view'));
+
+  useEffect(() => {
+    const v = zCalendarView.safeParse(searchParams.get('view'));
+    if (v.success) {
+      setView(v.data);
+    }
+  }, [searchParams]);
 
   return view;
 }
