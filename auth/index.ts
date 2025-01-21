0b25a59d591f063a95d45b9a752ef7a04560033f
JacobSchroder <36925561+JacobSchroder@users.noreply.github.com>
2025-01-21 13:12:58 +0100

    next-auth drizzle adapter setup

--- auth/index.ts
+++ auth/index.ts
@@ -1,4 +1,12 @@
 import { env } from '@/env';
+import { db } from '@/server/db';
+import {
+  accountsTable,
+  sessionsTable,
+  usersTable,
+  verificationTokensTable,
+} from '@/server/db/schema';
+import { DrizzleAdapter } from '@auth/drizzle-adapter';
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
 import Google from 'next-auth/providers/google';
@@ -9,4 +17,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub, Google],
   basePath: BASE_PATH,
   secret: env.AUTH_SECRET,
+  adapter: DrizzleAdapter(db, {
+    accountsTable: accountsTable,
+    sessionsTable: sessionsTable,
+    usersTable: usersTable,
+    verificationTokensTable: verificationTokensTable,
+  }),
 });
